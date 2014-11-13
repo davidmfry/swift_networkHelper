91a416c93df1ec6ed8cf55c8aad323874daa3002
David Fry <david.fry.tv@gmail.com>
2014-11-13 08:23:14 -0800

    Initial Commit

---
+++ NetworkHelper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NetworkHelper
+//
+//  Created by David on 11/13/14.
+//  Copyright (c) 2014 David Fry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
